508af5fe51d78342563936f86e40359a2d748c4c
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2022-10-01 10:14:31 -0400

    Add files via upload

---
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -0,0 +1,214 @@
+
+
+import pandas as pd
+import yfinance as yf
+import datetime
+import time
+import requests
+import io
+import torch
+import streamlit as st
+import pandas_datareader as pdr
+from datetime import datetime
+import pypfopt
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+from pypfopt import HRPOpt
+from pypfopt.efficient_frontier import EfficientCVaR
+from PIL import Image
+from random import randint
+from pypfopt import HRPOpt
+image = Image.open('C:/Users/Domin/Downloads/optGraph.jpg')
+st.image(image,caption = 'Portfolio Optimization', use_column_width = True)
+    
+    
+
+#%%
+
+from datetime import date
+from datetime import timedelta
+
+
+def create_portfolio(tick_list, start_date, end_date): 
+  if '-' in start_date: 
+    start_date.replace('-', '/')
+  if '-' in end_date: 
+    end_date.replace('-', '/')
+  start_date = datetime.strptime(start_date, '%m/%d/%Y')
+  end_date = datetime.strptime(end_date, '%m/%d/%Y')
+
+  df_list = []
+  for i in tick_list: 
+    stock = yf.download(i, start = start_date, end = end_date, progress = False)
+    if len(stock) == 0: 
+      continue 
+
+    else: 
+      stock.rename( columns = {'Close': i}, inplace= True)
+      stock = stock[i]
+      df_list.append(stock)
+
+
+  portfolio_list = pd.concat(df_list, axis = 1)
+  portfolio_list.dropna(inplace= True)
+  return portfolio_list
+
+
+
+
+
+#Mean Variance Optimization
+
+
+def MVO_opt(portfolio):
+   
+    mu = mean_historical_return(portfolio)
+    S = CovarianceShrinkage(portfolio).ledoit_wolf()
+    
+    ef = EfficientFrontier(mu, S)
+       
+    weights = ef.max_sharpe()
+    cleaned_weights = ef.clean_weights()
+    
+    cleaned_weights = pd.DataFrame().append(dict(cleaned_weights), ignore_index = True).T.reset_index()
+    cleaned_weights.columns = ['Ticker', 'Allocation']
+    st.text('')
+    st.markdown('**Portfolio Performance**')
+    st.write('Expected Annual Return:           ',str(round(ef.portfolio_performance()[0] * 100, 3)) + '%')
+    st.write('Annual Volatility:           ',str(round(ef.portfolio_performance()[1] * 100, 3)) + '%')
+    st.write('Sharpe Ratio:           ',str(round(ef.portfolio_performance()[2], 3)))
+    st.text('')
+    st.text('')
+    
+    
+    
+    latest_prices = get_latest_prices(portfolio)
+    da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=port_value)
+    allocation, leftover = da.greedy_portfolio()
+    allocation = pd.DataFrame().append(dict(allocation), ignore_index = True).T.reset_index()
+    allocation.columns = ['Ticker', 'Number of stocks']
+    st.markdown("**Discrete stock allocation:**")
+    st.text('')
+    st.dataframe(allocation)
+    st.write(("Funds remaining: ${:.2f}".format(leftover)))
+    st.text('')
+
+    
+    
+
+
+# %%
+
+#Hierarchal Risk Parity:
+def HRP(portfolio):
+    mu = mean_historical_return(portfolio)
+    S = CovarianceShrinkage(portfolio).ledoit_wolf()
+    returns = portfolio.pct_change().dropna()
+    
+    hrp = HRPOpt(returns)
+    hrp_weights = hrp.optimize()
+    hrp.portfolio_performance(verbose=True)
+    st.text('')
+    st.markdown('**Portfolio Performance**')
+    st.write('Expected Annual Return:           ',str(round(hrp.portfolio_performance()[0] * 100, 3)) + '%')
+    st.write('Annual Volatility:           ',str(round(hrp.portfolio_performance()[1] * 100, 3)) + '%')
+    st.write('Sharpe Ratio:           ',str(round(hrp.portfolio_performance()[2], 3)))
+    st.text('')
+    st.text('')
+    
+    latest_prices = get_latest_prices(portfolio)
+    da_hrp = DiscreteAllocation(hrp_weights, latest_prices, total_portfolio_value= port_value)
+    
+    allocation, leftover = da_hrp.greedy_portfolio()
+    allocation = pd.DataFrame().append(dict(allocation), ignore_index = True).T.reset_index()
+    allocation.columns = ['Ticker', 'Number of stocks']
+    print("Discrete allocation (HRP):", allocation)
+    print("Funds remaining (HRP): ${:.2f}".format(leftover))
+    
+    
+    
+
+    
+    da = DiscreteAllocation(hrp_weights, latest_prices, total_portfolio_value = port_value)
+    
+    st.markdown("**Discrete stock allocation:**")
+    st.text('')
+    st.dataframe(allocation)
+    st.write(("Funds remaining: ${:.2f}".format(leftover)))
+    st.text('')
+
+
+
+#Mean Conditional Value at Risk
+
+
+
+def MCV(portfolio): 
+
+    mu = mean_historical_return(portfolio)
+    S = CovarianceShrinkage(portfolio).ledoit_wolf()
+    returns = portfolio.pct_change().dropna()
+    ef_cvar = EfficientCVaR(mu, S)
+    cvar_weights = ef_cvar.min_cvar()
+    cleaned_weights = ef_cvar.clean_weights()
+    
+   
+    
+    latest_prices = get_latest_prices(portfolio)
+    da_cvar = DiscreteAllocation(cvar_weights, latest_prices, total_portfolio_value=port_value)
+    
+    allocation, leftover = da_cvar.greedy_portfolio()
+    allocation = pd.DataFrame().append(dict(allocation), ignore_index = True).T.reset_index()
+    allocation.columns = ['Ticker', 'Number of stocks']
+    st.write("Discrete allocation:")
+    st.text('')
+    st.dataframe(allocation)
+    st.write(("Funds remaining: ${:.2f}".format(leftover)))
+    st.text('')
+
+
+
+ticker_str = st.text_input('Input your list of tickers. Format must follow: "Ticker1, Ticker2, Ticker3..."')
+stock_list = [i.strip() for i in list(ticker_str.split(','))]
+selected_stocks = st.multiselect('Chosen Stocks: ', options  = stock_list, default = stock_list)
+
+
+start_date = st.text_input('Write your stock start date in month/day/year format', '09/01/2020')
+end_date = st.text_input('Write your stock end date in month/day/year format', '09/01/2022')
+
+
+
+if "" not in selected_stocks  and start_date != False and end_date != False:
+    portfolio = create_portfolio(selected_stocks, start_date, end_date)
+    
+    #st.write(type(portfolio))
+    if portfolio is None:
+        st.text('')
+        st.write('Invalid Stock Selection')
+        st.stop()
+        
+    portfolio.to_csv("portfolio.csv")
+    portfolio = pd.read_csv("portfolio.csv")    
+    portfolio.index = portfolio['Date']
+    portfolio.drop(['Date'], inplace = True, axis = 1)
+    st.write(portfolio)
+    port_value = st.text_input('What amount do you plan on investing in your portfolio?')
+    if port_value == '' or port_value is None: 
+        st.stop()
+    else:
+        port_value = float(port_value)
+        opt_list = ['None Selected', 'Mean Variance Optimization', 'Hierarchal Risk Parity', 'Mean Conditional Value at Risk'] 
+        choice = st.selectbox('Choose Which Optimization Technique You Would Like To Use: ', opt_list)
+        
+        if choice == opt_list[1]: 
+           MVO_opt(portfolio)
+        elif choice == opt_list[2]:
+            HRP(portfolio)
+        elif choice == opt_list[3]: 
+            MCV(portfolio)
+else: 
+    st.stop()
+
+    
