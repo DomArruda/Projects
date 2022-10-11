ed9bbb571c627f635ff42f7f83729d4b45e50231
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2022-10-11 07:41:48 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -193,8 +193,7 @@ def MCV(portfolio):
 
 ticker_str = st.text_input('Input your list of tickers. Format must follow: "Ticker1, Ticker2, Ticker3..."')
 stock_button = st.button('Not sure what to pick? Download S&P 500 stocks')
-st.write(stock_button)
-if stock_button != None: 
+if stock_button != False: 
     URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     ticker_list = pd.read_html(URL)[0]['Symbol'].tolist()
 stock_list = [i.strip() for i in list(ticker_str.split(','))]
