4c6f7c5ff3caf231fc3c0834968ef8ec4bc0b1b8
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2022-10-11 07:42:33 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -197,7 +197,7 @@ if stock_button != False:
     URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     ticker_list = pd.read_html(URL)[0]['Symbol'].tolist()
 stock_list = [i.strip() for i in list(ticker_str.split(','))]
-selected_stocks = st.multiselect('Chosen Stocks: ', options  = ticker_list, default = stock_list)
+selected_stocks = st.multiselect('Chosen Stocks: ', options  = stock_list, default = stock_list)
 
 
 
