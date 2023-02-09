097794b953da548048274514e08eea3b90a4719a
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-02-09 10:39:45 -0500

    Update sentiment.py

--- SentimentApp/sentiment.py
+++ SentimentApp/sentiment.py
@@ -10,15 +10,24 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from PIL import Image
 import csv
 import plotly.express as px
+import pyodbc
 #%%
 
-tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+@st.cache(allow_output_mutation = True)
 
-model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-
+def load_model(boolean): 
+    if boolean is True: 
+        model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+        return model 
+@st.cache(allow_output_mutation = True)
+def load_tokenizer(boolean): 
+    if boolean is True: 
+        tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+        return tokenizer
 #%% 
 
-
+model = load_model(True)
+tokenizer = load_tokenizer(True)
 
 def sentiment_score(review):
     tokens = tokenizer.encode(review, return_tensors='pt')
@@ -61,7 +70,20 @@ def reviews_csv(reviews):
         data['score'] = data[selected_column].apply(lambda x: sentiment_score(x[-512:])) 
         return data
     
-
+def SQL_scrape(query, conn): 
+    data = pd.read_sql(query, conn)
+    data_columns = data.columns 
+    data_columns = data_columns.insert(0, 'None Selected')
+    selected_column = st.selectbox('Which column contains the reviews?', data_columns)
+    totalReviews = len(data)
+    st.text(f'Total number of reviews detected: {totalReviews}')
+    numReviews = int(st.number_input('How many reviews should I analyze? '))
+    if selected_column == data_columns[0] or numReviews <= 0 or numReviews > totalReviews  : 
+        st.stop()
+    else:
+        data = data.iloc[:numReviews]
+        data['score'] = data[selected_column].apply(lambda x: sentiment_score(x[-512:])) 
+        return data
 
 st.title('Jim Bot :robot_face:')
 with st.sidebar.header('**Upload your CSV data.**'):
@@ -75,6 +97,14 @@ with st.sidebar.header('Upload reviews via website link: '):
 with st.sidebar.header('Upload the number of pages to grab reviews from'):
     num_pages = int(st.number_input('Enter the number of pages to scrape:', step = 1))
 
+
+with st.sidebar.header('Connect to SQL: '): 
+    st.header('Connect to SQL')
+with st.sidebar.header('Connect to SQL: '):
+   server =  st.text_input('Please enter the name of the server: ')
+
+with st.sidebar.header('Please enter the name of the database: '):
+    database =  st.text_input('Please enter the name of the database: ')
 
 
 
@@ -107,9 +137,38 @@ elif uploaded_link != '' and num_pages is not None and uploaded_file is None:
     st.download_button('Click here to download your sentiment report', 
                        review_csv, 'sentiment.csv')
     
+elif  server != '' and database != '' and uploaded_file is None and uploaded_link == '':
+    try: 
+        server_login = f'Server={server};'
+        database_login = f'Database={database};'
+        connection_login = f'Trusted_Connection=yes;'
     
+        login = 'Driver={SQL Server};' + server_login + database_login + connection_login
+        conn = pyodbc.connect(login)
+        cursor = conn.cursor()
+        
+        query = st.text_area('Please enter your query here')
+        if query == '': 
+            st.stop()
+        else: 
+            try:
+                review_data = SQL_scrape(query, conn)
+                review_data.reset_index(inplace= True, drop = True)
+                st.dataframe(review_data, use_container_width= True)
+                fig = px.histogram(review_data,  x= 'score', title = 'Histogram of Scores')
+                
+                fig.update_traces(marker_line_color = 'white', marker_line_width = 1.0)
+                
+                st.plotly_chart(fig)
+                review_csv = review_data.to_csv(index = False).encode('utf-8')
+                st.download_button('Click here to download your sentiment report', 
+                                   review_csv, 'sentiment.csv')
+                
+            except: 
+                st.text('')
+             
+    except: 
+         st.text('Encountered an error')
+
     
-    
-else: 
-    st.stop()
 
