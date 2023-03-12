0e23a44ed90bc557fe743ea22691743aeaaa4aab
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-03-12 09:41:02 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -240,10 +240,10 @@ if "" not in selected_stocks  and start_date != False and end_date != False:
         portfolioData = portfolio.to_csv(index = True).encode('utf-8')
         st.download_button('Click Here To Download Stock Data', 
                        portfolioData, 'StockData.csv')
-    
-        corr_option = st.selectbox(
-            'Pick Correlation Method: ',
-            ['pearson', 'kendall', 'spearman'])
+    '''
+    Bit buggy for some reason
+    '''
+        corr_option = st.selectbox('Pick Correlation Method: ',['pearson', 'kendall', 'spearman'])
         fig = plx.imshow(portfolio.corr(method = 'corr_option').round(2), title = f'Stock Correlations: - {corr_option}', text_auto = True)
         st.plotly_chart(fig)
         port_value = st.text_input('What amount do you plan on investing in your portfolio?')
