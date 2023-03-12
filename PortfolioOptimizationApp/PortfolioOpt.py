77e8974f661d299b2a9dea055258e65ee856f7b5
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-03-12 09:47:54 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -246,7 +246,7 @@ if "" not in selected_stocks  and start_date != False and end_date != False:
         corr_option = st.selectbox( 'Pick Correlation Method: ',correlation_types)
           
   
-        fig = plx.imshow(portfolio.corr(method = corr_option).round(2), title = f'Stock Correlations: - {corr_option}', text_auto = True)
+        fig = plx.imshow(portfolio.corr(method = corr_option).round(2), title = f'Stock Correlations- {corr_option.title()}:', text_auto = True)
         st.plotly_chart(fig)
         port_value = st.text_input('What amount do you plan on investing in your portfolio?')
         if port_value == '' or port_value is None: 
