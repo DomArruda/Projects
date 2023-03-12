8dece3f03ba25349618381ecb4048670d06e7781
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-03-12 09:33:14 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 09:29:19 2023
+
+@author: darruda
+"""
+
 
 
 import pandas as pd
@@ -210,7 +217,7 @@ selected_stocks = st.multiselect('Chosen Stocks: ', options  = stock_list, defau
 
 
 
-start_date = st.text_input('Write your stock start date in month/day/year format', (datetime.today() - timedelta(days = 1 + (2 * 365) ).strftime('%m/%d/%Y'))
+start_date = st.text_input('Write your stock start date in month/day/year format', (datetime.today() - timedelta(days = 2 * 366)).strftime('%m/%d/%Y'))
 end_date = st.text_input('Write your stock end date in month/day/year format', (datetime.today() - timedelta(days = 1)).strftime('%m/%d/%Y'))
 
 
@@ -234,7 +241,7 @@ if "" not in selected_stocks  and start_date != False and end_date != False:
         st.download_button('Click Here To Download Stock Data', 
                        portfolioData, 'StockData.csv')
     
-        corr_ option = st.selectbox(
+        corr_option = st.selectbox(
             'Pick Correlation Method:?',
             ('pearson', 'kendall', 'spearman'))
         fig = plx.imshow(portfolio.corr(method = 'corr_option').round(2), title = 'Stock Correlations:', text_auto = True)
