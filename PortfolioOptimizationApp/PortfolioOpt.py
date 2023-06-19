2dfb0ab145fe2ec43b8681de1f37556820fb89c5
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-06-19 10:29:31 -0400

    Update PortfolioOpt.py

--- PortfolioOptimizationApp/PortfolioOpt.py
+++ PortfolioOptimizationApp/PortfolioOpt.py
@@ -382,7 +382,7 @@ if ("" not in selected_stocks)  and (start_date != False) and (end_date != False
                     st.text(e_)
                     #st.text('Error Occured - Please try again')
     except Exception as e_2 : 
-            st.text(e_2)
+            st.text("An error has occurred - Please try again and check your inputs. )
       
 else: 
     st.stop()
