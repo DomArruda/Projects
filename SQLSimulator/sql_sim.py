9d11fa7749cfabaa1cbb5c51bfec74a46ee8ad25
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-10-23 12:34:03 -0400

    Update sql_sim.py

--- SQLSimulator/sql_sim.py
+++ SQLSimulator/sql_sim.py
@@ -1,40 +1,40 @@
-
 import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine
+from pyspark.sql import SparkSession
+from PIL import Image
+image = Image.open(r"C:\Users\darruda\Downloads\database_schema.png")
+spark = SparkSession.builder.appName("SQL_Sim").getOrCreate()
 st.title("SQL SIMULATOR ⚙️")
+st.image(image, caption = '', use_column_width = True)
 
-st.markdown('## Please write a query from the SALES table into the text input below')
-st.text('')
-st.text('')
-@st.cache
 def read_data():
-    data = pd.read_csv(r"SQLSimulator/SaleTransactions.csv")
-    if 'index' in list(data.columns):
-        data = data.drop('index', axis = 1)
-    return data
+    df1 = spark.read.csv(r"C:\Users\darruda\Downloads\Salespeople.csv", header=True, inferSchema=True)
+    df2 = spark.read.csv(r"C:\Users\darruda\Downloads\Salespeople_Data.csv", header=True, inferSchema=True)
+    df3 = spark.read.csv(r"C:\Users\darruda\Downloads\Transactions.csv", header=True, inferSchema=True)
+    return df1, df2, df3
 
-data = read_data()
-engine = create_engine('sqlite://', echo=False)
-data.to_sql(name = 'Sales', con = engine, index = False)
-conn = engine.connect()
+salespeople, sales, transactions= read_data()
+@st.cache
+def create_tables():
+    salespeople.createOrReplaceTempView("Salespeople")
+    sales.createOrReplaceTempView("Sales")
+    transactions.createOrReplaceTempView("Transactions")
+
+create_tables()
 button_bool = st.button("CLICK HERE TO GRAB THE FIRST 5 ROWS")
 
-def query_database(conn, query):
-
-    df = pd.read_sql(query, conn)
-    #execution_result = conn.execute(query)
-    #df = pd.DataFrame(execution_result.fetchall())
-    #df.columns = execution_result.keys()
+def query_database(query):
+    df = spark.sql(query)
     return df
 
 if button_bool: 
-    df = query_database(conn = engine, query = "SELECT * FROM SALES LIMIT 5")
+    df = query_database(query = "SELECT * FROM SALES LIMIT 5")
     st.markdown("*Query: SELECT * FROM SALES LIMIT 5*")
-    st.dataframe(df)
+    st.dataframe(df.toPandas())
     button_bool = False
-    st.markdown(f'*Number of rows:         {df.shape[0]:,}*')
-    st.markdown(f'*Number of cols:         {df.shape[1]:,}*')
+    st.markdown(f'*Number of rows:         {df.count():,}*')
+    st.markdown(f'*Number of cols:         {len(df.columns):,}*')
     st.text('')
     st.text('')
     
@@ -43,13 +43,9 @@ query = st.text_area(label = "Write your SQL Query here")
 if str(query) != '':
     try: 
         st.text('')
-        df = query_database(conn = engine, query = query)
-        st.dataframe(df, use_container_width = False)
-        st.markdown(f'*Number of rows:         {df.shape[0]:,}*')
-        st.markdown(f'*Number of cols:         {df.shape[1]:,}*')
+        df = query_database(query = query)
+        st.dataframe(df.toPandas(), use_container_width = False)
+        st.markdown(f'*Number of rows:         {df.count():,}*')
+        st.markdown(f'*Number of cols:         {len(df.columns):,}*')
     except: 
         st.markdown("Oops! Looks like we've encountered an error. Try checking your query")
-
-
-
-    
