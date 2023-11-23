73bf69655601acfc46468df55e86e10028326b9d
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2023-11-23 10:55:53 -0500

    Update sentiment_app.js

--- JavascriptML/sentiment_app.js
+++ JavascriptML/sentiment_app.js
@@ -2,9 +2,8 @@ async function query(data) {
 	const response = await fetch(
 		"https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment",
 		{
-			headers: { Authorization: "PUT YOUR OWN API TOKEN HERE" },
-            // https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment?inference_api=true
-			method: "POST",
+			headers: { Authorization: "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxx" }, // YOU WILL HAVE TO USE YOUR OWN API KEY....
+            			method: "POST",
 			body: JSON.stringify(data),
 		}
 	);
@@ -14,10 +13,6 @@ async function query(data) {
 
 async function getResult(input) {
     let response = await query({"inputs": input});
-    let firstObject = response[0][0];
-    let numStars = firstObject['label'];
-    let probScore = parseFloat(firstObject['score']).toFixed(2) * 100;
-    console.log(`Predicted Score: ${numStars}\nProbability: ${probScore}%`);
     return response;
 }
 
@@ -26,5 +21,12 @@ document.getElementById('ml-form').addEventListener('submit', async function(e) 
     let userInput = document.getElementById('user-input').value;
     let result = await getResult(userInput);
     let resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `<br><strong>\n\nPredicted Score: ${result[0][0].label}<br>Probability: ${(result[0][0].score * 100).toFixed(1)}%</strong>`;;
+    let html = '<br><strong>Probabilities:</strong><br>';
+    let sortedResult = result[0].sort((a, b) => parseInt(a.label) - parseInt(b.label)); // we want it so that 1 star always shows first...
+    for (let obj of sortedResult) {
+        let numStars = obj['label'];
+        let probScore = Math.floor(parseFloat(obj['score']).toFixed(2) * 100);
+        html += `${numStars}: ${probScore}%<br>`;
+    }
+    resultDiv.innerHTML = html;
 });
