a0dbf6de11744a30077309f7644e8271043a0bb8
Dom Arruda <81713385+DomArruda@users.noreply.github.com>
2024-02-26 14:53:37 -0500

    Update sql_sim.py

--- SQLSimulator/sql_sim.py
+++ SQLSimulator/sql_sim.py
@@ -58,13 +58,14 @@ def CreateQuery():
     
     if str(query['text']) != '':
         try:
-            st.text('')
-            df = query_database(query = query['text'].lower())
-            st.dataframe(df, use_container_width = False)
-            st.markdown(f'*Number of rows:{df.shape[0]:,}*')
-            st.markdown(f'*Number of cols:{df.shape[1]:,}*')
-        except Exception as e:
-            st.markdown(f"Oops! Looks like we've encountered an error. Try checking your query. (Error: {e})")
+            for query in query['text'].split(";")
+                st.text('')
+                df = query_database(query = query['text'].lower())
+                st.dataframe(df, use_container_width = False)
+                st.markdown(f'*Number of rows:{df.shape[0]:,}*')
+                st.markdown(f'*Number of cols:{df.shape[1]:,}*')
+            except Exception as e:
+                st.markdown(f"Oops! Looks like we've encountered an error. Try checking your query. (Error: {e})")
 
 
 if __name__ == "main"
